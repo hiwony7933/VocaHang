7877d3d45b2457035f2439c0ab21fef0681d1cb7
leejoowon <zero@MacBook-Pro.local>
2025-04-24 14:45:19 +0900

    린트 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,19 @@
 // .eslintrc.cjs
 module.exports = {
   root: true,
-  extends: "@react-native",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
     extraFileExtensions: [".html"],
   },
-  ignorePatterns: ["*.html"],
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["*.html", "node_modules/", "dist/"],
+  rules: {
+    "no-console": "off",
+  },
 };
